f83dab9e82ab5cce7692eff8871adc698d308b08
Your Name <lokeshtiwari212@gmail.com>
2024-01-29 12:31:38 +0530

    fixed

--- Docs-App/src/Components/Cards.jsx
+++ Docs-App/src/Components/Cards.jsx
@@ -1,25 +1,31 @@
 import React from 'react'
 import { FaRegFileLines } from "react-icons/fa6";   //React-icons from react icon website   
 import { AiOutlineDownload } from "react-icons/ai";
+import { IoMdClose } from "react-icons/io";
 
 
-function Cards() {
+function Cards({data}) {
   return (
-    <div className=' relative w-60 h-72 rounded-[45px] bg-zinc-900/90 text-white py-10 px-8 overflow-hidden'>
+    <div className=' relative flex-shrink-0 w-60 h-72 rounded-[45px] bg-zinc-900/90 text-white py-10 px-8 overflow-hidden'>
 <FaRegFileLines/>
 
-<p className=' text-sm leading-tight font-semibold mt-5'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis ullam placeat deserunt quasi!</p>
+<p className=' text-sm leading-tight font-semibold mt-5'>{data.desc}</p>
 
 <div className='footer absolute  w-full bottom-0 left-0 '>
 <div className='flex items-center justify-between  px-8 py-3 mb-3'>
-    <h5>.4mb</h5>
+    <h5>{data.filesize}</h5>
     <span className='rounded-full bg-zinc-600 w-7 h-7 flex items-center justify-center'>
-    <AiOutlineDownload size=".7em" color="#fff"/>
+      {data.close ? <IoMdClose/>: <AiOutlineDownload size=".7em" color="#fff"/>}
+   
     </span>
 </div>
+{
+  data.tag.isOpen && (
 <div className='tag width-full py-4 bg-green-600 flex items-center justify-center'>
    <h3 className='text-sm font-semibold '>Download Now</h3>
 </div>
+  )}
+
     </div>
     </div>
   )
--- Docs-App/src/Components/Foreground.jsx
+++ Docs-App/src/Components/Foreground.jsx
@@ -1,10 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Cards from './Cards'
 
 function Foreground() {
+  const data = [
+ {
+  desc : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis ullam placeat deserunt quasi!", 
+  filesize:".9mb", 
+  close: true, 
+  tag: {isOpen:true, tagTitle:"Download Now", tagColor:"green"}
+},
+
+{
+  desc : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis ullam placeat deserunt quasi!", 
+  filesize:".9mb", 
+  close: true, 
+  tag: {isOpen:true, tagTitle:"Download Now", tagColor:"green"}
+},
+
+{
+  desc : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis ullam placeat deserunt quasi!", 
+  filesize:".9mb", 
+  close: true, 
+  tag: {isOpen:true, tagTitle:"Download Now", tagColor:"green"}
+},
+  ];
+  useState()
+
   return (
-   <div className='fixed top-0 left-0 w-full h-screen z-[3]'>
-    <Cards/>
+   <div className='fixed top-0 left-0 w-full h-full z-[3] flex gap-10 flex-wrap p-5'>
+    {data.map((item, index)=>(
+      <Cards data={item}/>
+    ))}
      
    </div>
   )
